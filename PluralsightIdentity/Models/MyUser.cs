35667d74eaf158bf85fcc59daa819c4c963a2897
stanlylife <stianhave@hotmail.com>
2020-04-23 13:01:36 +0200

    added UserStorage

--- PluralsightIdentity/Models/MyUser.cs
+++ PluralsightIdentity/Models/MyUser.cs
@@ -9,7 +9,6 @@ namespace PluralsightIdentity.Models {
 		public string Id { get; set; }
 		public string UserName { get; set; }
 		public string NormalizedUsername { get; set; }
-
 		public string PasswordHash { get; set; }
 	}
 }
--- PluralsightIdentity/Startup.cs
+++ PluralsightIdentity/Startup.cs
@@ -10,25 +10,30 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace PluralsightIdentity {
+
 	public class Startup {
+
 		public Startup(IConfiguration configuration) {
 			Configuration = configuration;
 		}
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddControllersWithViews();
+
+			services.AddIdentityCore<string>(options => {
+				options.Password.RequiredLength = 1;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireDigit = false;
+			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
 			if (env.IsDevelopment()) {
 				app.UseDeveloperExceptionPage();
 			} else {
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 			app.UseHttpsRedirection();
