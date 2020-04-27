0d07f602c134add00a3e075e5b843371b20439e9
stanlylife <stianhave@hotmail.com>
2020-04-27 11:51:44 +0200

    added custom password validator

---
+++ Identity 3/PluralsightIdentity/Models/ForgotPassword.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PluralsightIdentity.Controllers {
+
+	public class ForgotPassword {
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+	}
+}
---
+++ Identity 3/PluralsightIdentity/Models/ResetPasswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PluralsightIdentity.Controllers {
+
+	public class ResetPasswordModel {
+		public string Token { get; set; }
+
+		public string Email { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Compare("Password")]
+		public string ConfirmPassword { get; set; }
+	}
+}
